a1f7704f1ad840144de4ebedb54aa60f4b73c3b1
Efren David Alarcón Muñoz <efrendavidalarconmunoz@gmail.com>
2025-12-13 12:34:41 -0500

    Clean up comments in ProfileImage component
    Removed comments related to image effects and requirements.

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -5,13 +5,9 @@ export default function ProfileImage() {
   return (
     <div className="image-wrapper-center">
       <div className="image-card">
-        {/* Efecto de resplandor trasero */}
         <div className="image-glow"></div>
-
-        {/* Marco e imagen */}
         <div className="image-frame">
           <img
-            // Asegúrate de tener esta imagen en la carpeta public
             src="/professional-profile-photo-of-developer.jpg" 
             alt="Efren David Alarcon Muñoz"
             className="profile-img"
